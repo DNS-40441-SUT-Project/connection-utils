d034bb02ab1d0074b6954db6a794909c2f74fcec
Amirsadra Abdollahi <abdollahiamirsadra@gmail.com>
2023-09-29 12:53:54 +0330

    Update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 setuptools.setup(
     include_package_data=True,
     name='connection_utils',
-    version='0.2.3.0.1',
+    version='0.2.4',
     packages=setuptools.find_packages(),
     install_requires=['rsa', 'cryptography'],
 )
